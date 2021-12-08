8d766b4be8002680c4c5e8f4293d20098794a201
SNT <SNT@DESKTOP-JRIMABO>
2021-12-08 13:23:50 +0900

    board 컨트롤러, 서비스단 커밋

--- src/main/java/com/dyKim/domain/board/BoardService.java
+++ src/main/java/com/dyKim/domain/board/BoardService.java
@@ -4,9 +4,11 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.dyKim.domain.board.mapper.BoardMapper;
 
+@Service
 public class BoardService {
 
 	@Autowired
